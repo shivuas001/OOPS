3a5be03d352ec0c89cd8904ff6d61eba3336a18d
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-13 19:07:16 +0530

    methods concept in java

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,22 +1,27 @@
 package oopt1;
 
-class Calculator{
-	
-	public int add(int n1, int n2) {
-		int r=n1+n2;
-		return r;
+class Computer{
+	 
+	public void playMusic() {
+		System.out.println("music playing...");
+		}
+	public String getMePen(int cost) {
+	if(cost >= 10)
+		return "pen";
+	else
+		return "i will not give";
 	}
 }
+
 
 public class Demo {
 
 	public static void main(String[] args) {
-		int num1 = 4;
-		int num2 = 3;
 		
-		Calculator calc = new Calculator();
-		int result = calc.add(num1, num2);
-		System.out.println(result);
+		Computer obj1 = new Computer();
+		obj1.playMusic();
+		String str = obj1.getMePen(5);
+		System.out.println(str);
 	}
 
 }
