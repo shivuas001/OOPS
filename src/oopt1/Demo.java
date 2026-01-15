f1b48c9a04b3708469ff8ab05afe8515fa66f7d5
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-15 11:44:04 +0530

    arrays2

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -4,9 +4,15 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		int nums[] = {3,4,5,9};
-		nums[2] = 23;
-		System.out.println(nums[2]);
+		int nums[] = new int[4];
+		nums[0] = 5;
+		nums[1] = 3;
+		nums[2] = 2;
+		nums[3] = 9;
+		
+		for(int i=0;i<4;i++) {
+			System.out.println(nums[i]);
+		}
 		
 	}
 
