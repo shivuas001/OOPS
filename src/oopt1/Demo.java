2bd68e565df5955607ce6a05a2ab36423a282fb9
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-15 19:36:43 +0530

    Jagged array

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -4,25 +4,19 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		int nums [][] = new int [3][4];
+	//Jagged array where each array with different sizes
+		int nums [][] = new int [3][];
+		nums[0] = new int[2];
+		nums[1] = new int[4];
+		nums[2] = new int[3];
+		
 		
 	//to generate a random number for an array of MultiDimensional array
-		for(int i=0;i<3;i++){
-			for(int j=0;j<4;j++) {
+		for(int i=0;i<nums.length;i++){
+			for(int j=0;j<nums[i].length;j++) {
 				nums[i][j] = (int)(Math.random() * 10);
 			}
 		}
-		
-	// Normal for loop to print an array of MultiDimentional array
-		
-		for(int i=0;i<3;i++){
-			for(int j=0;j<4;j++) {
-				System.out.print(nums[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
 		
 	//enhanced for loop to print MultiDimentional array
 		
