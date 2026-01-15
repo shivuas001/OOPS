629a47db9940b000c2c0ae5b32b13c734ef9f331
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-15 11:19:43 +0530

    arrays

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,24 +1,12 @@
 package oopt1;
 
-class Calculator{
-	 
-	 public int add(int n1, int n2, int n3) {
-		 int result = n1 + n2 + n3;
-		 return result;
-	 }
-	 public int add(int n1, int n2) {
-		 return n1 + n2;
-	 }
-}
-
-
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		Calculator calc = new Calculator();
-		int result = calc.add(3, 5);
-		System.out.println(result);
+		int nums[] = {3,4,5,9};
+		nums[2] = 23;
+		System.out.println(nums[2]);
 		
 	}
 
