de1ad8fa992cc088a807676b4bc1ffa200697f3a
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-15 12:48:19 +0530

    MultiDimentional Array

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -4,16 +4,36 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		int nums[] = new int[4];
-		nums[0] = 5;
-		nums[1] = 3;
-		nums[2] = 2;
-		nums[3] = 9;
+		int nums [][] = new int [3][4];
 		
-		for(int i=0;i<4;i++) {
-			System.out.println(nums[i]);
+	//to generate a random number for an array of MultiDimensional array
+		for(int i=0;i<3;i++){
+			for(int j=0;j<4;j++) {
+				nums[i][j] = (int)(Math.random() * 10);
+			}
 		}
 		
+	// Normal for loop to print an array of MultiDimentional array
+		
+		for(int i=0;i<3;i++){
+			for(int j=0;j<4;j++) {
+				System.out.print(nums[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+	//enhanced for loop to print MultiDimentional array
+		
+		for(int n[]: nums) {
+			for(int m: n) 
+			{
+				System.out.print(m + " ");
+			}
+			System.out.println();
+		}
 	}
-
+		
 }
+
