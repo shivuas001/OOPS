5ad2390c748c1abb805e52ec74f652eb4d984d0f
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-16 19:14:35 +0530

    enhanced for loop for basic array

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -10,33 +10,41 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		Students s1 = new Students();
-		s1.rollno = 23;
+	/*	Students s1 = new Students();
 		s1.name = "shivu";
-		s1.marks = 100;
+		s1.rollno = 24;
+		s1.marks = 44;
 		
 		Students s2 = new Students();
-		s2.rollno = 25;
-		s2.name = "rakesh";
-		s2.marks = 140;
+		s2.name = "shivu";
+		s2.rollno = 24;
+		s2.marks = 44;
 		
 		Students s3 = new Students();
-		s3.rollno = 26;
-		s3.name = "santosh";
-		s3.marks = 57;
+		s3.name = "shivu";
+		s3.rollno = 24;
+		s3.marks = 44;
 		
 		Students Students[] = new Students[3];
 		Students[0] = s1;
 		Students[1] = s2;
 		Students[2] = s3;
 		
-		for(int i=0;i<Students.length;i++)
+	//using Enhanced for loop	
+		for(Students S : Students)
 		{
-			System.out.println(Students[i].name + " : " + Students[i].rollno + " : " + Students[i].marks);
+			System.out.println(S.name + " : " + S.rollno);
+		} */
+		
+		int nums[] = new int[3];
+		nums[0] = 2;
+		nums[1] = 4;
+		nums[2] = 7;
+		
+		for(int n : nums)
+		{
+			System.out.println(n);
 		}
-		
-		
-		
 	}
 	
 }
