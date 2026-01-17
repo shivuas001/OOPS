94a5e48c3a5ea4ce7103746382fd14ae6f4446af
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-17 19:45:14 +0530

    Mutable vs Immutable in string

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -4,19 +4,20 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		//string is an object this how it works 
-		String name = new String("A SHIVA KUMAR");
-		System.out.println(name + " from HALEHALLI");
+		String name = "shivu";
+		name = name + "kumar";
 		
+	//here string does not changed from shivu to shivu kumar.
+	//heap as allocated different memory to name with of previous one.
+	//hence Strings are default immutable that is cant be changed.
+		System.out.println(name);
 		
-		// these are some methods since it is class. to check char at index 3 and concat two strings
-		System.out.println(name.charAt(3));
-		System.out.println(name.concat(" YADAV"));
+		String s1 = "Shivu";
+		String s2 = "Shivu";
 		
-		
-		//But this is the synatx for string as jvm automatically creates the object 
-		String Name = "RAKESH";
-		System.out.println(Name);
+	//it returns true bcoz s1 and s1 have same string name,
+	//so memory allocation in heap for both variable are same address.
+		System.out.println(s1 == s2);
 	}
 	
 }
