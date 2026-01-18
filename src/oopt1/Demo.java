148a5193af4c07db0c52ec03777af66046c89f71
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-18 18:49:20 +0530

    StringBuffer Updated

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -3,7 +3,12 @@ package oopt1;
 public class Demo {
 
 	public static void main(String[] args) {
+		
+	//basically capacity is 16 but if string size 5 then extra 5 spaces
+	//will be added in heap memory to relocate memory while data is changed
 		StringBuffer sb = new StringBuffer("SHIVA");
+		
+	//these different times of methods we can perform in string buffer
 		System.out.println(sb.capacity());
 		
 		System.out.println(sb.length());
