a8173472faef575b488c5fd1e2aa4a2185db0a51
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-19 19:12:06 +0530

    Static Block

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -3,33 +3,31 @@ package oopt1;
 class Mobile{
 	String brand;
 	int price;
-//its is static variable which shared by all objects as name in both objects are same 
 	static String name;
-	
-	public void display() {
-		System.out.println(brand + " : " + price + " : " + name);
+
+//Static Block to mention static variable
+	static
+	{
+		name = "";
+		System.out.println("static block called");
 	}
+//Constructor
+	public Mobile()
+	{
+		brand = "";
+		price = 200;
+		System.out.println("Constructor block called");
+	}
+	
 }
 
 public class Demo {
 
 	public static void main(String[] args) {
-		
+	//we have two objects so two times the constructor will call
+	//but has name var is static we want call it once so static block is used
 		Mobile obj1 = new Mobile();
-		obj1.brand = "redmi";
-		obj1.price = 12000;
-	//correct type to call static variable is by its class name not by its objects name bcoz they share same name
-		Mobile.name = "smartphone";
-		
 		Mobile obj2 = new Mobile();
-		obj2.brand = "vivo t3";
-		obj2.price = 22000;
-		Mobile.name = "smartphone";
-		
-		Mobile.name = "phone";
-		
-		obj1.display();
-		obj2.display();
 		
 	
 	}
