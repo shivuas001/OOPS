fc9fb87bbbb27e6727201e5ce63cb9dc6b98cbb5
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-20 18:43:43 +0530

    Static method

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -5,34 +5,38 @@ class Mobile{
 	int price;
 	static String name;
 
-//Static Block to mention static variable
-	static
+	public void display()
 	{
-		name = "";
-		System.out.println("static block called");
+		System.out.println(brand + " : " + price + " : " + name);
 	}
-//Constructor
-	public Mobile()
+	
+//non static variables cannot be called in static method so we need to the which obj to be called	
+	public static void display1(Mobile obj1)
 	{
-		brand = "";
-		price = 200;
-		System.out.println("Constructor block called");
+		System.out.println(obj1.brand + " : " + obj1.price + " : " + name);
 	}
 	
 }
 
 public class Demo {
 
-	public static void main(String[] args) throws ClassNotFoundException {
-	
-		//when we want to create object, class is loaded from library and object is created.
-		//when class loads, first static block is called. then object is created and constructor is called
-		
-		//when u want to load class but u dont have object.....
-		
-		//here it loads class to call static block.its an method to load class.
-		Class.forName("oopt1.Mobile");
-		 
+	public static void main(String[] args) {
+	     Mobile obj1 = new Mobile();
+	     obj1.brand = "redmi";
+	     obj1.price = 34033;
+	     Mobile.name = "phone";
+	     
+	     Mobile obj2 = new Mobile();
+	     obj2.brand = "redmi 4a";
+	     obj2.price = 545422;
+	     Mobile.name = "phone";
+	     
+	     obj1.display();
+	     obj2.display();
+	     
+	   //we need to call this method using class bcoz it is static method 
+	     Mobile.display1(obj1);
+	     
 	
 	}
 	
