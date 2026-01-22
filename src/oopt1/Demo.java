3b46ead2fe59417a87bafcd80a1ea5e8613bd904
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-22 18:48:38 +0530

    this keyword

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -12,11 +12,11 @@ class Human{
 	public String getName() {
 		return name;
 	}
-	public void setAge(int a) {
-		age = a;
+	public void setAge(int age) {
+		this.age = age;
 	}
-	public void setName(String n) {
-		name = n;
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
