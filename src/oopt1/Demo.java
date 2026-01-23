b69bbfcf6d67ea096457143c6a5894c5dcea1de6
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-23 18:27:48 +0530

    Super method

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,39 +1,43 @@
 package oopt1;
 
-class Human{
-	private int age;
-	private String name ;
-	
-	//Default Constructor
-	public Human(){
-	   name = "shivu";
-	   age = 12;
+class A{
+//by default super method will be in constructor to refer super class  
+	public A()
+	{
+		super();
+		System.out.println("in  A constroctor");
 	}
 	
-	//Parameterized Constructor as values assigned in object when its created
-	public Human(int age, String name){
-		  this.age = age;
-		  this.name = name;
-		}
-	
-	public int getAge() {
-		return age;
-	}
-	public String getName() {
-		return name;
+	public A(int n)
+	{
+		super();
+		System.out.println("in  A int constrctor");
 	}
 }
 
+class B extends A{
+	public B()
+	{
+		super();
+		System.out.println("in B cons");
+	}
+	
+//here when u use parametric constructor when u extend A class an default constructor is called in super class
+//and we two constructor in super class that is A. 
+//when u pass an parameters in super method then the parametric constructor of A also will be called orelse an default will be called
+	public B(int n)
+	{
+		super(n);
+		System.out.println("in B int cons");
+	}
+}
 
 public class Demo {
 
 	public static void main(String[] args) {
-	    Human obj = new Human();
-	    Human obj2 = new Human(15,"madhu");
-	    System.out.println(obj.getName() + " : " + obj.getAge());
-	    System.out.println(obj2.getName() + " : " + obj2.getAge());
-	    
+	    B obj = new B(5);
 	}
 	
 }
+
 
