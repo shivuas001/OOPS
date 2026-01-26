b582c08239ac75926a21c8cf6b8862bcc0bfa75b
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-26 20:06:26 +0530

    packages1

--- src/oopt2/Demo1.java
+++ src/oopt2/Demo1.java
@@ -2,11 +2,16 @@ package oopt2;
 
 //here we are importing the file that is present in other package that is from oopt1 folder and file Calc
 import oopt1.Calc;
+import oopt1.Advcalc;
+
+//here it imports, selects java folder inside it util folder and selects all files in it.
+import java.util.*;
+
 
 
 //now after importing inheritance program
 
-class AdvanceCalc extends Calc{
+class AdvanceCalc extends Advcalc{
 	public int mod(int n1,int n2){
 		{
 			return n1%n2;
@@ -17,12 +22,15 @@ class AdvanceCalc extends Calc{
 public class Demo1 {
 
 	public static void main(String[] args) {
+		
 		AdvanceCalc obj = new AdvanceCalc();
 		int r1=obj.add(5, 6);
 		int r2=obj.sub(5, 6);
 		int r3=obj.mod(5, 6);
+		int r4=obj.multi(5, 6);
+		int r5=obj.mod(5, 6);
 		
-		System.out.println(r1 + " " + r2 + " " + r3);
+		System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
 		
 
 	}
