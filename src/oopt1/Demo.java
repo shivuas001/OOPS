fbd6ad208ba7d99c2454aed7dde627172bc0f44e
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-26 11:18:21 +0530

    inheritance

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,27 +1,19 @@
 package oopt1;
 
-class A{
-	
-	public void show()
-	{
-		System.out.println("in show method");
-	}
-	
-	public A()
-	{
-		System.out.println("in Constructor");
-	}
-}
-
 
 public class Demo {
 
 	public static void main(String[] args) {
-	// we call call the methoud using this anonymous object
-	//but cant reuse it with other methods
-	    new A().show();  
+
+		Advcalc obj = new Advcalc();
+		int r1 = obj.add(5, 12);
+		int r2 = obj.sub(12, 5);
+		int r3 = obj.multi(5, 2);
+		int r4 = obj.div(25, 5);
+		
+		System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+		
 	}
-	
 }
 
 
