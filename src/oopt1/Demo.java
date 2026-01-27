b5da369ebcb50eb324783255568ef7e71c5f7c87
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-27 22:13:24 +0530

    Access modifiers

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,9 +1,13 @@
 package oopt1;
+import oopt2.*;
+//here we can access marks that is in other package we have imported that package 
 
 public class Demo {
 
 	public static void main(String[] args) {
-
+          A obj = new A();
+       //we access the marks only when it is made public in other package
+          System.out.println(obj.marks);
 	}
 }
 
---
+++ src/oopt2/A.java
@@ -0,0 +1,7 @@
+package oopt2;
+
+public class A {
+	//if this variable is public then it can be used in any packages
+       public int marks = 6;
+       
+}
