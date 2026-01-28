cce59939c47f5acfca8148d0db63de51166c344f
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-28 18:29:05 +0530

    Dynamic method Dispatch 2

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -13,14 +13,29 @@ class Ba extends Aa{
 	}
 }
 
+class Ca extends Ba{
+	public void show()
+	{
+		System.out.println("in C show");
+	}
+}
+
 
 public class Demo {
 
 	public static void main(String[] args) {
 		
-	//here we have different reference of Aa but object is Ba and this works
-         Aa obj = new Ba();
+	//here same method is behaving differently according to the object we refer	
+         Aa obj = new Aa();
          obj.show();
+     //here obj is changed from Aa to Ba 
+         obj = new Ba();
+         obj.show();
+     //here obj is changed from Ba to Ca
+         obj = new Ca();
+         obj.show();
+         
+        //this is decided in runtime where same method behave different according to object
 	}
 }
 
