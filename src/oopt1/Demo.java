79c5ac4ad348182484124b83d6d6df1bd4d2eaeb
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-29 18:30:23 +0530

    Typecasting

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,31 +1,14 @@
 package oopt1;
 
-class  Calculator1{
-//when we make method as final we can't override that method if we override it through an error
-	public final void show()
-	{
-		System.out.println("by Shivakumar");
-	}
-	public void add(int a, int b)
-	{
-		System.out.println(a+b);
-	}
-}
-
-class AdvCalc1 extends Calculator1
-{
-	public void show()
-	{
-		System.out.println("by chirag");
-	}
-	}
-
 public class Demo {
 
 	public static void main(String[] args) {
-		Calculator1 obj = new Calculator1();
-	   obj.show();
 	
+	   double d = 4.5;
+	  //TypeCasting converting double to integer to store in integer variable
+	   int i = (int) d;
+	   
+	   System.out.println(i);
 	}
 }
 
