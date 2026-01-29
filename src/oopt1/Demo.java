23b6c495ac0b9233824dbc84c832432c2997ca11
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-29 19:06:39 +0530

    Downcasting

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -16,13 +16,18 @@ class Bb extends Aa{
 public class Demo {
 
 	public static void main(String[] args) {
-		
-	//this is upcasting as object is Bb it refers up that is parent class Aa
-	//we need not mention this upcast as it default and this happens in backend just for understanding
 	   Aa obj = (Aa) new Bb();
 	   obj.show1();
-	 
-	}
+	   
+	//Downcasting 
+	//there is problem that we created object of Bb but we unable to call show2() though it belongs to Bb class
+	//this is because object Bb have an reference of Aa in object and Aa class  doen't know Bb 
+	   
+	   Bb obj1 = (Bb) obj;
+	   obj1.show2();
+	 //now we can call show2() method because we downcasted Aa 
+	 //we created another reference Bb of variable obj1 and downcasted obj of refence Aa is from Bb
+   }
 }
 
 
