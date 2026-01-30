b4e237c35064a2933e1b01cf04bc24cd0909281a
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-30 19:18:25 +0530

    need of interface

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,21 +1,41 @@
 package oopt1;
 
-abstract class A{
-	public abstract void show();
+abstract class Computer{
+	public abstract void code(); 
+}
+
+class Desktop extends Computer{
+	public void code()
+	{
+		System.out.println("Compile, run, debug: Faster");
+	}
+}
+
+class Laptop extends Computer{
+	public void code()
+	{
+		System.out.println("Compile, run, debug");
+	}
+}
+
+class Developer{
+//here we are passing Computer object parameter as both laptop and Desktop extends Computer
+	public void devApp(Computer lap)
+	{
+		lap.code();
+	}
 }
 
 public class Demo {
 	public static void main(String[] args) {
-	//when we have only one abstract method to define in other other class instead using anonymous inner class it works
-	// we can also use if we have multiple abstract method.
-		A obj = new A() 
-		{
-			public void show() 
-			{
-				System.out.println("in new show");
-			}
-		};
-		obj.show();
+		//here we have Computer reference and laptop and desktop as object
+		  Computer lap = new Laptop();
+		  Computer des = new Desktop();
+		  
+		  
+		  Developer shivu = new Developer();
+		  shivu.devApp(des);
+		 //here when we pass des it call devapp method where this method took Computer as reference calls Desktop method.
 		  
    }
 }
