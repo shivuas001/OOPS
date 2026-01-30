bd2cf00f5a2b4d96c0e7398c6fa38c5f4de4c4e5
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-30 19:32:02 +0530

    interfaces

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,25 +1,24 @@
 package oopt1;
 
-abstract class Computer{
-	public abstract void code(); 
+interface Computer{
+	void code();
 }
 
-class Desktop extends Computer{
+class Laptop implements Computer{
 	public void code()
 	{
-		System.out.println("Compile, run, debug: Faster");
+		System.out.println("compile, run, debug");
+	}
+}
+class Desktop implements Computer{
+	public void code()
+	{
+		System.out.println("compile, run, debug : Faster");
 	}
 }
 
-class Laptop extends Computer{
-	public void code()
-	{
-		System.out.println("Compile, run, debug");
-	}
-}
 
 class Developer{
-//here we are passing Computer object parameter as both laptop and Desktop extends Computer
 	public void devApp(Computer lap)
 	{
 		lap.code();
@@ -28,15 +27,11 @@ class Developer{
 
 public class Demo {
 	public static void main(String[] args) {
-		//here we have Computer reference and laptop and desktop as object
-		  Computer lap = new Laptop();
-		  Computer des = new Desktop();
-		  
-		  
-		  Developer shivu = new Developer();
-		  shivu.devApp(des);
-		 //here when we pass des it call devapp method where this method took Computer as reference calls Desktop method.
-		  
+		Computer lap = new Laptop();
+		Computer des = new Desktop();
+		
+		Developer shivu = new Developer();
+		shivu.devApp(des);
    }
 }
 
