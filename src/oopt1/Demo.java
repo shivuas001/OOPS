7f08f5f293a377021b8db13a265ec1bc9fc2d4a8
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-31 12:18:43 +0530

    Enum using if else

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,22 +1,24 @@
 package oopt1;
 
-//here Status is a class and components inside it are constants
 enum Status{
 	Running, Failed, Pending, Success
 }
 public class Demo {
 	public static void main(String[] args) {
 	
-	//values is of type array so mentioning array in reference is must if not leads to error
-	   Status[] ss = Status.values();
-	   
-	//Enhanced for loop we used is to print all the status constants and
-	//ordinal() method to print a specific values of particular constant
-	   
-	  for(Status s: ss) {
-		  System.out.println(s + " : " + s.ordinal());
-	  }
+	  Status s = Status.Success;
 	  
+	  if(s == Status.Running) {
+		  System.out.println("All Good");
+	  }
+	  else if(s == Status.Failed) {
+		  System.out.println("Try Again");
+	  }
+	  else if(s == Status.Pending) {
+		  System.out.println("Please Wait");
+	  }
+	  else
+		  System.out.println("Done");  
    }
 }
 
