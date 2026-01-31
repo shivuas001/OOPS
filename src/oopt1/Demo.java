db417dcaa1882393895e53cfd658261025f1444a
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-31 19:53:53 +0530

    Annotation in Java

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,38 +1,28 @@
 package oopt1;
 
-enum Laptop{
-//if we wont mention price of one object we can use default constructor
-     Macbook(2000), Vivobook(1000), Hp, Ideapad(1200);
-	
-	 private int price;
-	 
-	 private Laptop() {
-	//now default price value for Hp will be 500.
-		 price = 500;
-	 }
-	 
-	 private Laptop(int price) {
-		 this.price = price;
-	 }
-	 
-	 public int getPrice()
-	 {
-		 return price;
-	 }
-	 public void setPrice(int price)
-	 {
-		 this.price = price;
-	 }
+class A{
+	public void show()
+	{
+		System.out.println("in A show");
+	}
 }
+class Bb extends A{
+//@Override is an Annotation and we have many in build annotations in java and we can create of our own.
+//annotation are basically to supply extra information to compile to help in fixing bugs
+//@Override whill try inform compiler i am try to override help to solve bugs.here i did not use proper method to override it shows error
+     @Override
+	public void shov()
+	{
+		System.out.println("in B show");
+	}
+}
+
 public class Demo {
 	public static void main(String[] args) {
 	 
-	//to print all objects with prices Enhanced for loop used
-		for(Laptop lap : Laptop.values())
-		{
-			System.out.println(lap + " : " + lap.getPrice());
-		}
-		
+		Bb obj = new Bb();
+		obj.show();
+	
    }
 }
 
