e3363e10b60e0cc2a05f22aa81899ddebb1c25d7
A SHIVA KUMAR <shivuas605@gmail.com>
2026-01-31 19:25:11 +0530

    Enum Class3

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,10 +1,15 @@
 package oopt1;
 
 enum Laptop{
-//all these are objects in enum
-     Macbook(2000), Vivobook(1000), Hp(1500), Ideapad(1200);
+//if we wont mention price of one object we can use default constructor
+     Macbook(2000), Vivobook(1000), Hp, Ideapad(1200);
 	
 	 private int price;
+	 
+	 private Laptop() {
+	//now default price value for Hp will be 500.
+		 price = 500;
+	 }
 	 
 	 private Laptop(int price) {
 		 this.price = price;
