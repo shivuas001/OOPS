6269f4b005c1ae26f97dfcc2c57fb4c37cfb3c7d
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-01 11:35:17 +0530

    Lambda Expression using Return

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -3,17 +3,28 @@ package oopt1;
 //when FuntionalInterface annotation is used it gives error when more than methods are found in interface.
 @FunctionalInterface 
 interface A{
-	void show(int i);
+	int add(int a, int b);
 }
 
 
 public class Demo {
 	public static void main(String[] args) {
-		//this is updated lambda expression using FuntionalInterface Annotation by passing parameters
-		//here in bracket we need not mention type of variable lambda expression will look after it in JVM.
+	
+	//now we used here is add() method and we passed parameters we see in line one 
+	//that a + b there we need not mention return type bcoz lambda know it and in bracket no need mention type of variable.
+		A obj = (a,b) ->  a + b;
+		int result = obj.add(5, 12);
+		System.out.println(result);
 		
-		A obj = (i) ->  System.out.println("in show() method" + " : " + i);
-		obj.show(5);
+	//inner class code without lambda Expression
+		
+	/*	A obj = new A() {
+			public int add(int a, int b) {
+				return a+b;
+			}
+		}; 
+		int result = obj.add(5, 12);
+		System.out.println(result);   */
    }
 }
 
