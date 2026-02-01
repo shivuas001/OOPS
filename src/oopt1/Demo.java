356b773021803a5a0ca68b8af3a347a4ac0eaa37
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-01 10:52:02 +0530

    @FuntionalInterface annotation

--- src/oopt1/Demo.java
+++ src/oopt1/Demo.java
@@ -1,26 +1,20 @@
 package oopt1;
 
-class A{
-	public void show()
-	{
-		System.out.println("in A show");
-	}
+//when FuntionalInterface annotation is used it gives error when more than methods are found in interface.
+@FunctionalInterface 
+interface A{
+	void show();
 }
-class Bb extends A{
-//@Override is an Annotation and we have many in build annotations in java and we can create of our own.
-//annotation are basically to supply extra information to compile to help in fixing bugs
-//@Override whill try inform compiler i am try to override help to solve bugs.here i did not use proper method to override it shows error
-     @Override
-	public void shov()
-	{
-		System.out.println("in B show");
-	}
-}
+
 
 public class Demo {
 	public static void main(String[] args) {
-	 
-		Bb obj = new Bb();
+	 //we used Anonymous inner class here
+		A obj = new A() {
+			public void show() {
+				System.out.println("in show() method");
+			}
+		};
 		obj.show();
 	
    }
