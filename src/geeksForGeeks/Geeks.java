de1977bc4491d7f8de12cb4b77eba4a47602b8d8
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-05 07:26:02 +0530

    ArrayList(adding,removing,updating)

--- src/geeksForGeeks/Geeks.java
+++ src/geeksForGeeks/Geeks.java
@@ -7,14 +7,23 @@ public class Geeks {
 	public static void main(String[] args) {
 		
 		//creating arraylist and elements
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<String> list = new ArrayList<>();
 		 
-		 list.add(20);
-		 list.add(40);
-		 list.add(22);
+		 list.add("A");
+		 list.add("KUMAR");
+		 System.out.println("after adding"  + " : " + list);
 		 
-		 System.out.println(list);
-
+		 list.add(1,"SHIVA");
+		 System.out.println("after adding element at index 1" + " : " + list);
+		 
+		 list.remove(0);
+		 System.out.println("removes index 0 element" + " : " + list);
+		 
+		 list.remove("KUMAR");
+		 System.out.println("removing elemnts using value" + " : " + list);
+		 
+		 list.set(0,"Hello");
+		 System.out.println("to remove the element from index 0 and replace it given string" + " : " +list);
 	}
 
 }
